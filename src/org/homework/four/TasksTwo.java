37d0a31e975e32b8c7e770457e00c7c74ef090e3
VoytovychM <151627487+VoytovychM@users.noreply.github.com>
2024-01-22 16:59:25 +0100

    nested cicle

---
+++ src/org/homework/four/TasksTwo.java
@@ -0,0 +1,48 @@
+package org.homework.four;
+
+public class TasksTwo {
+    public static void main(String[] args) {
+
+//    }
+//     for (int i = 1; i <= 100; i++) {
+//        if (i % 3 == 0 && i % 5 == 0) {
+//            System.out.println(i + " = FizzBuzz");
+//        }
+//        else if (i % 3 == 0) {
+//            System.out.println(i + " = Fizz");
+//        } else if (i % 5 == 0) {
+//            System.out.println(i + " = Buzz");
+//        }
+//        else{
+//            System.out.println(i);
+//        }
+//        int height = 14;
+//        for (int i = 0; i <= height; i++) {
+//            if (i == 0){
+//            System.out.println(1);
+//            }
+//            System.out.print(1);
+//            for (int x = 0; x < i; x++) {
+//                System.out.print(0);
+//            }
+//            System.out.println(1);
+//        }
+//
+
+        int heights1 = 14;
+        for (int i = 14; i >= 0 ; i--) {
+            System.out.print(1);
+            for (int x = 0; x < i; x++) {
+                System.out.print(0);
+            }
+            if(i != 0) {
+                System.out.println(1);
+            }
+
+
+
+
+        }
+    }
+}
+
